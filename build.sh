e7e17d1bb7f5e66392df62fe578e67d6f041b94d
Mendgart444 <mendgart.hauschildt+github@gmail.com>
2025-08-25 12:43:25 +0000

    added some corrections in build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 g++ src/main.cpp -o bin/data-sort-mini
-x86_64-w64-mingw32-g++ src/main.cpp -o bin/data-sort-mini.exe
+x86_64-w64-mingw32-g++ src/*.cpp -o bin/data-sort-mini.exe -static -static-libgcc -static-libstdc++
