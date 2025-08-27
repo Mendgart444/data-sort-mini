12e3c8b479bf7c43fd2052f6076efff0701d9aff
Mendgart444 <mendgart.hauschildt+github@gmail.com>
2025-08-27 07:54:00 +0000

    Desing updates! added console colors and an code clean up version 0.0.2 is out!

--- src/main.cpp
+++ src/main.cpp
@@ -4,34 +4,74 @@
 #include <algorithm>
 #include <set>
 
-#define ver "0.0.1\n"
+// often used values
+#define ver "0.0.2\n"
+//colors
+#define red "\e[0;31m"
+#define green "\e[0;32m"
+#define yellow "\e[0;33m"
+#define blue "\e[0;34m"
+// color reset
+#define reset "\e[0m"
 
 namespace fs = std::filesystem;
 
-int main(int argc, char** argv) {
-    std::cout << "Data Sort Mini Tool " << ver;
+// This function sorts the files in folders. It uses the file extension like main.cpp to sort them.
+//                                                                                ~~~
+int sort(std::map<std::string, std::string> file_extension_category, std::string path_to_sort) {
+    try {
+        for (const auto& current_dir : fs::directory_iterator(path_to_sort)) {
+            if (current_dir.is_regular_file()) {
+                std::string file_extension = current_dir.path().extension().string();
 
+                std::transform(file_extension.begin(), file_extension.end(), file_extension.begin(), ::tolower);
+
+                if (file_extension_category.count(file_extension)) {
+                    std::string target_folder = path_to_sort + "/" + file_extension_category[file_extension];
+                    fs::create_directories(target_folder);
+
+                    fs::path target_path = target_folder + "/" + current_dir.path().filename().string();
+                    fs::rename(current_dir.path(), target_path);
+
+                    std::cout << blue << "[INFO] " << reset << "Move: " << current_dir.path().filename() << blue
+                        << " -> " << reset << target_folder << "\n";
+                }
+            }
+        }
+    } catch (std::exception& e) {
+        std::cerr << red << "Error: " << e.what() << reset << "\n";
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char** argv) {
+    // little welcome text.
+    std::cout << blue << "Data Sort Mini Tool " << reset << ver;
+
+    // Error if no arguments delivered.
     if (argc < 2) {
         std::cerr << "Error: please provide a path";
         return 1;
     }
-
-    std::string arg = argv[1];
-
-    if (arg == "-v") {
+    // take first argument.
+    std::string arg1 = argv[1];
+    // parse arg1.
+    if (arg1 == "-v") {
         std::cout << "version: " << ver;
-    } else if (arg == "-h") {
+    } else if (arg1 == "-h") {
         std::cout << "data-sort-mini tool version: " << ver << "Usage: data-sort-mini <DIR> --exclude* .cpp .pdf\n\n";
         
         std::cout << "commands:\n";
         std::cout << "-v: display version\n";
         std::cout << "-h: display help text\n\n";
 
-        std::cout << "* = optional\n"
+        std::cout << "* = optional\n";
     } else {
-        std::string path = arg;
-        std::string arg1 = argv[2];
-        std::map<std::string, std::string> category = {
+        std::string arg2 = argv[2];
+        // this variabel is needed for the sort() function.
+        std::map<std::string, std::string> file_extension_category = {
             {".jpg", "Pictures"},
             {".png", "Pictures"},
             {".pdf", "Documents"},
@@ -61,43 +101,26 @@ int main(int argc, char** argv) {
             {".cat", "Catalogs"},
             {".dll", "Software Extensions"},
         };
+        // When the user is using the --exclude argument.
         std::set<std::string> exclude;
-
-        if (arg1 == "--exclude") {
+        // detect if the user uses the exclude argument and handle it.
+        if (arg2 == "--exclude") {
             for (int i = 3; i < argc; i++) {
                 exclude.insert(argv[i]);
             }
 
             for (const auto& key : exclude) {
-                category.erase(key);
+                file_extension_category.erase(key);
             }
         }
-
-        try {
-            for (const auto& entry : fs::directory_iterator(path)) {
-                if (entry.is_regular_file()){
-                    std::string end = entry.path().extension().string();
-
-                    std::transform(end.begin(), end.end(), end.begin(), ::tolower);
-
-                    if (category.count(end)) {
-                        std::string targetFolder = path + "/" + category[end];
-                        fs::create_directories(targetFolder);
-
-                        fs::path zielPfad = targetFolder+ "/" + entry.path().filename().string();
-                        fs::rename(entry.path(), zielPfad);
-
-                        std::cout << "[INFO] Move: " << entry.path().filename() 
-                                    << " → " << targetFolder << std::endl;
-                    }   
-                }
-            }
-        } catch (const std::exception& e) {
-            std::cerr << "Error: " << e.what() << "\n";
-            return 1;
+        // catch the exit code.
+        int result_of_sort = sort(file_extension_category, arg1);
+        // parse the exit code.
+        if (result_of_sort == 0) {
+            std::cout << green << "Sucess!\n" << reset;
+            return 0;
+        } else {
+            return result_of_sort;
         }
     }
-    std::cout << "Sucess!\n";
-
-    return 0;
 }
